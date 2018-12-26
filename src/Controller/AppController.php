a873679449b8d61cead4523d1b403b1a6683c670
tomek-servlets <tomek.servlets@gmail.com>
2018-12-26 19:41:05 +0100

    Next info

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -51,5 +51,8 @@ class AppController extends Controller
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
          */
         //$this->loadComponent('Security');
+
+        $this->response = $this->response->cors($this->request)
+            ->allowOrigin(['*.*'])->build();
     }
 }
