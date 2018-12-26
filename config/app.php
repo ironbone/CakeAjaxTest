59294458b741975c2b70e6b62c29444747343cf7
tomek-servlets <tomek.servlets@gmail.com>
2018-12-26 17:34:21 +0100

    Next info

--- config/app.php
+++ config/app.php
@@ -255,8 +255,8 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'cakephp',
-            'password' => 'cakephp',
+            'username' => 'root',
+            'password' => 'root',
             'database' => 'default',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
