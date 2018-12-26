c3470f9aad408fbf9b909438868295aebc5800e7
tomek-servlets <tomek.servlets@gmail.com>
2018-12-26 18:11:15 +0100

    Next info

--- config/app.php
+++ config/app.php
@@ -258,6 +258,7 @@ return [
             'username' => 'root',
             'password' => 'root',
             'database' => 'sampledb',
+
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
